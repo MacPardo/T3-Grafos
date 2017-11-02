964708020a74dcce2dfb18f46bcced77fcd5a5e8
Marcos TG Adamczuk <marcos.adamczuk@gmail.com>
2017-11-02 20:44:26 -0200

    kruskal done

--- main.cpp
+++ main.cpp
@@ -1,24 +1,59 @@
 #include <iostream>
+#include <tuple>
+#include <set>
 #include <vector>
 #include <utility>
-#include <set>
 #include <functional>
 
 using namespace std;
-typedef vector<set<pair<int, int> > > graph;
+typedef vector<vector<pair<int, int> > > graph;
 
 bool has_cycle(graph g, int v) {
-  bool answer;
   bool ok[g.size()] = {false};
-  function<bool (int, int)> dfs = [&dfs, &ok, g](int v, int p) -> bool {
+  function<bool (int, int)> dfs = [&dfs, &ok, &g](int v, int p) -> bool {
     if (ok[v]) return true;
     ok[v] = true;
-    for (set<pair<int, int> >::iterator it = g[v].begin(); it != g[v].end(); it++) {
+    for (vector<pair<int, int> >::iterator it = g[v].begin(); it != g[v].end(); it++) {
       if (it->second != p && dfs(it->second, v)) return true;
     }
     return false;
   };
   return dfs(v, -1);
+}
+
+graph kruskal(graph g) {
+  uint32_t added_edges = 0;
+  set<tuple<int, int, int> > edges;
+  graph k(g.size());
+  for (uint32_t i = 0; i < g.size(); i++) {
+    for (vector<pair<int, int> >::iterator it = g[i].begin(); it != g[i].end(); it++) {
+      if (edges.find(tuple<int, int, int>(it->first, i, it->second)) == edges.end()) {
+        edges.insert(tuple<int, int, int>(it->first, it->second, i));
+      }
+    }
+  }
+
+  for (set<tuple<int, int, int> >::iterator it = edges.begin(); it != edges.end() && added_edges < g.size() - 1; it++) {
+    graph test = k;
+    test[get<1>(*it)].push_back(pair<int, int>(get<0>(*it), get<2>(*it)));
+    test[get<2>(*it)].push_back(pair<int, int>(get<0>(*it), get<1>(*it)));
+    if (!has_cycle(test, get<1>(*it))) {
+      added_edges++;
+      k[get<1>(*it)].push_back(pair<int, int>(get<0>(*it), get<2>(*it)));
+      k[get<2>(*it)].push_back(pair<int, int>(get<0>(*it), get<1>(*it)));
+    }
+  }
+  return k;
+}
+
+void prt_grafo(graph g) {
+  for (uint32_t i = 0; i < g.size(); i++) {
+    cout << i << ": ";
+    for (vector<pair<int, int> >::iterator it = g[i].begin(); it != g[i].end(); it++) {
+      cout << it->second << ' ';
+    }
+    cout << '\n';
+  }
 }
 
 int main(void) {
@@ -28,16 +63,13 @@ int main(void) {
   graph g(v);
   while (e--) {
     cin >> a >> b >> w;
-    g[a].insert(pair<int, int>(w, b));
-    g[b].insert(pair<int, int>(w, a));
+    g[a].push_back(pair<int, int>(w, b));
+    g[b].push_back(pair<int, int>(w, a));
   }
-  for (int i = 0; i < g.size(); i++) {
-    printf("%d: ", i);
-    for (set<pair<int, int> >::iterator it = g[i].begin(); it != g[i].end(); it++) {
-      printf("%d ", it->second);
-    }
-    printf("\n");
-  }
-  printf("%s\n", has_cycle(g, 0) ? "true" : "false");
+  prt_grafo(g);
+  cout << (has_cycle(g, 0) ? "true" : "false") << '\n';
+
+  graph k = kruskal(g);
+  prt_grafo(k);
   return 0;
 }
